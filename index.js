6d39360c5ceb6d4db559e706761dd23d1254da47
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-05-15 16:40:16 +0500

    use chalk & animation

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
 #! /usr/bin/env node
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter first Number", type: "number", name: "firstNumber" },
-    { message: "Enter Second Number", type: "number", name: "secondNumber" },
-    {
-        message: "select one of the operators to perform operation",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "subtraction", "multiplication", "division"],
-    },
-]);
-//conditional statement
-if (answer.operator === "Addition") {
-    console.log("Your value is", answer.firstNumber + answer.secondNumber);
+console.log(chalk.red("*******************************************")); //ckalk animation start
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow('WelCome To Calculator Project');
+    await sleep();
+    //   rainbowTitle.stop();
 }
-else if (answer.operator === "subtraction") {
-    console.log("Your value is", answer.firstNumber - answer.secondNumber);
+;
+await welcome();
+console.log(chalk.red("*******************************************")); // chalk animation end
+let condition = true;
+while (condition) {
+    const answer = await inquirer.prompt([
+        { message: "Please Enter first Number", type: "number", name: "firstNumber" },
+        { message: "Please Enter Second Number", type: "number", name: "secondNumber" },
+        {
+            message: "select one of the operators to perform operation",
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        },
+    ]);
+    //conditional statement
+    if (answer.operator === "Addition") {
+        console.log(chalk.green.underline("Your Addition result is", answer.firstNumber + answer.secondNumber));
+        console.log(chalk.yellow("*******************************************"));
+    }
+    else if (answer.operator === "Subtraction") {
+        console.log(chalk.yellow.underline("Your Subtraction result is", answer.firstNumber - answer.secondNumber));
+        console.log(chalk.red("*******************************************"));
+    }
+    else if (answer.operator === "Multiplication") {
+        console.log(chalk.magenta.underline("Your Multiplication result is", answer.firstNumber * answer.secondNumber));
+        console.log(chalk.whiteBright("*******************************************"));
+    }
+    else if (answer.operator === "Division") {
+        console.log(chalk.red.underline("Your Division result is", answer.firstNumber / answer.secondNumber));
+        console.log(chalk.green("*******************************************"));
+    }
+    else {
+        console.log("select valid operator");
+    }
+    ;
+    let con = await inquirer.prompt({
+        name: "continue",
+        type: "confirm",
+        message: "Do you wnat to continue ?",
+        default: "false",
+    });
+    condition = con.continue;
+    console.log(condition);
 }
-else if (answer.operator === "multiplication") {
-    console.log("Your value is", answer.firstNumber * answer.secondNumber);
+;
+console.log(chalk.green("*******************************************")); //ckalk animation
+const thankYou = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function bye() {
+    const rainbowTitle2 = chalkAnimation.rainbow('Thank You & Have a Good Day!');
+    await thankYou();
+    rainbowTitle2.stop();
 }
-else if (answer.operator === "division") {
-    console.log("Your value is", answer.firstNumber / answer.secondNumber);
-}
-else {
-    console.log("select valid operator");
-}
+;
+await bye();
+console.log(chalk.green("*******************************************"));
--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 #! /usr/bin/env node
 
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
 
+console.log(chalk.red("*******************************************"));//ckalk animation start
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'WelCome To Calculator Project'
+  );
+  await sleep();
+//   rainbowTitle.stop();
+};
+await welcome();
+console.log(chalk.red("*******************************************"));// chalk animation end
+
+let condition=true;
+while(condition){
 const answer = await inquirer.prompt([
   { message: "Please Enter first Number", type: "number", name: "firstNumber" },
   { message: "Please Enter Second Number", type: "number", name: "secondNumber" },
@@ -9,18 +25,45 @@ const answer = await inquirer.prompt([
     message: "select one of the operators to perform operation",
     type: "list",
     name: "operator",
-    choices: ["Addition", "subtraction", "multiplication", "division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
   },
 ]);
 //conditional statement
 if(answer.operator === "Addition"){
-    console.log("Your value is", answer.firstNumber + answer.secondNumber );
-}else if(answer.operator === "subtraction"){
-    console.log("Your value is", answer.firstNumber - answer.secondNumber );
-}else if(answer.operator === "multiplication"){
-    console.log("Your value is", answer.firstNumber * answer.secondNumber );
-}else if(answer.operator === "division"){
-    console.log("Your value is", answer.firstNumber / answer.secondNumber );
-}else{
+    console.log(chalk.green.underline("Your Addition result is", answer.firstNumber + answer.secondNumber ));
+    console.log(chalk.yellow("*******************************************"));
+  }else if(answer.operator === "Subtraction"){
+    console.log(chalk.yellow.underline("Your Subtraction result is", answer.firstNumber - answer.secondNumber ));
+    console.log(chalk.red("*******************************************"));
+  }else if(answer.operator === "Multiplication"){
+    console.log(chalk.magenta.underline("Your Multiplication result is", answer.firstNumber * answer.secondNumber ));
+    console.log(chalk.whiteBright("*******************************************"));
+  }else if(answer.operator === "Division"){
+    console.log(chalk.red.underline("Your Division result is", answer.firstNumber / answer.secondNumber ));
+    console.log(chalk.green("*******************************************"));
+  }else{
     console.log("select valid operator");
-}
+};
+
+let con=await inquirer.prompt({
+    name:"continue",
+    type:"confirm",
+    message:"Do you wnat to continue ?",
+    default:"false",
+},);
+
+condition=con.continue
+console.log(condition);
+};
+
+console.log(chalk.green("*******************************************"));//ckalk animation
+const thankYou = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function bye() {
+  const rainbowTitle2 = chalkAnimation.rainbow(
+    'Thank You & Have a Good Day!'
+  );
+  await thankYou();
+  rainbowTitle2.stop();
+};
+await bye();
+console.log(chalk.green("*******************************************"));
